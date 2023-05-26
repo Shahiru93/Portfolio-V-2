7fbb5f91f416f623271f24c983c33955558976cc
Shahiru Roshen <68189195+Shahiru93@users.noreply.github.com>
2023-05-26 20:24:54 +0530

    small changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,7 @@
 const navMenu = document.getElementById("nav-menu"),
       navToggle = document.getElementById("nav-toggle"),
       navClose = document.getElementById("nav-close")
-
-
-      var mybutton = document.getElementById("myBtn");
-
-      // When the user scrolls down 20px from the top of the document, show the button
-      window.onscroll = function() {scrollFunction()};
       
-      function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          mybutton.style.display = "block";
-        } else {
-          mybutton.style.display = "none";
-        }
-      }
-      
-      // When the user clicks on the button, scroll to the top of the document
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
 /*=============== SHOW MENU ===============*/
 /* validate if constant exists  */
 if(navToggle)
